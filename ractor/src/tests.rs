acd1f567038a0f05ee9b7a65ddeddf73809ae428
Sean Lawlor <slawlor@slawlor.com>
2023-05-05 09:35:27 -0400

    Adding a helper extraction method to RactorErr to quickly extract the message, if present (#100)

--- ractor/src/tests.rs
+++ ractor/src/tests.rs
@@ -5,6 +5,10 @@
 
 //! Basic tests of errors, error conversions, etc
 
+use crate::Actor;
+use crate::ActorCell;
+use crate::ActorProcessingErr;
+use crate::ActorRef;
 use crate::RactorErr;
 
 #[test]
@@ -31,4 +35,42 @@ fn test_error_conversions() {
         RactorErr::<()>::from(crate::MessagingErr::ChannelClosed).to_string(),
         other
     );
+}
+
+#[crate::concurrency::test]
+async fn test_error_message_extraction() {
+    struct TestActor;
+
+    #[async_trait::async_trait]
+    impl Actor for TestActor {
+        type Msg = ();
+        type State = ();
+        type Arguments = ();
+
+        async fn pre_start(
+            &self,
+            _: ActorRef<Self::Msg>,
+            _: Self::Arguments,
+        ) -> Result<Self::State, ActorProcessingErr> {
+            Ok(())
+        }
+    }
+
+    let (actor, handle) = Actor::spawn(None, TestActor, ())
+        .await
+        .expect("Failed to start test actor");
+    // stop the actor, and wait for death which will free the message channels
+    actor.stop(None);
+    handle.await.unwrap();
+
+    let err = crate::cast!(actor, ()).expect_err("Not an error!");
+    assert!(err.has_message());
+    assert!(err.try_get_message().is_some());
+
+    let cell: ActorCell = actor.into();
+    let bad_message_actor: ActorRef<u32> = cell.into();
+
+    let err = crate::cast!(bad_message_actor, 0u32).expect_err("Not an error!");
+    assert!(!err.has_message());
+    assert!(err.try_get_message().is_none());
 }
