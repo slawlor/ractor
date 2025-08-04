44fe73b053e5a60d4af37f8750ca1657e6503551
Sean Lawlor <seanlawlor@meta.com>
2025-08-04 10:02:33 -1000

    Update ractor to 0.15.7
    Semver checks are clean indicating no need for a major release with these changes
    
    ```
    $ cargo semver-checks -p ractor
        Building ractor v0.15.7 (current)
           Built [   8.412s] (current)
         Parsing ractor v0.15.7 (current)
          Parsed [   0.012s] (current)
        Building ractor v0.15.6 (baseline)
           Built [   7.274s] (baseline)
         Parsing ractor v0.15.6 (baseline)
          Parsed [   0.012s] (baseline)
        Checking ractor v0.15.6 -> v0.15.7 (minor change)
         Checked [   0.035s] 140 checks: 140 pass, 37 skip
         Summary no semver update required
        Finished [  20.651s] ractor
    ```

--- ractor/src/time.rs
+++ ractor/src/time.rs
@@ -232,7 +232,22 @@ where
     where
         F: Fn() -> TMessage + Send + 'static,
     {
-        send_interval::<TMessage, F>(period, self.get_cell(), msg)
+        // IMPORTANT: See notes on `send_interval` above for important implementation
+        // notes
+        let self_clone = self.clone();
+        crate::concurrency::spawn(async move {
+            let mut timer = crate::concurrency::interval(period);
+            // timer tick's immediately the first time
+            timer.tick().await;
+            while ACTIVE_STATES.contains(&self_clone.get_status()) {
+                timer.tick().await;
+                // if we receive an error trying to send, the channel is closed and we should stop trying
+                // actor died
+                if self_clone.send_message(msg()).is_err() {
+                    break;
+                }
+            }
+        })
     }
 
     /// Alias of [send_after]
@@ -244,7 +259,12 @@ where
     where
         F: FnOnce() -> TMessage + Send + 'static,
     {
-        send_after::<TMessage, F>(period, self.get_cell(), msg)
+        let self_clone = self.clone();
+        crate::concurrency::spawn(async move {
+            crate::concurrency::sleep(period).await;
+            let msg = msg();
+            self_clone.send_message(msg)
+        })
     }
 
     /// Alias of [exit_after]
