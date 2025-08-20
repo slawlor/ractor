26261cc1181d503f895e4eb9c0433bee828d4706
Sean Lawlor <seanlawlor@meta.com>
2025-08-20 07:26:43 -1000

    Remove message boxing for performance improvement
    This is an extension of the message boxing approach proposed in PR #370. This however continues the improvements to remove additional type information that isn't necessary.
    
    This diff includes the removal of the BoxedMessage struct in favor of a crate-private RactorMessage trait which adds the necessary extension methods, only within the `ractor`
    context so as the privatize that functionality, as it was an unintentional leak anyways. This of course is a semver break as a public type is going away, but it should have been unused
    anyways in all contexts. Additionally `BoxedDowncastErr` is renamed to `DowncastErr` as we are no longer boxing the message, but again this error variant shouldn't really be used publicly.
    
    Will re-run the performance benchmarks present in #370 and past as a PR comment once ready

--- ractor/benches/actor.rs
+++ ractor/benches/actor.rs
@@ -49,197 +49,109 @@ impl Actor for BenchActor {
 }
 
 fn create_actors(c: &mut Criterion) {
-    let small = 100;
-    let large = 10000;
-
-    let id = format!("Creation of {small} actors");
     #[cfg(not(feature = "async-std"))]
     let runtime = tokio::runtime::Builder::new_multi_thread().build().unwrap();
     #[cfg(feature = "async-std")]
-    let _ = async_std::task::block_on(async {});
-    c.bench_function(&id, move |b| {
-        b.iter_batched(
-            || {},
-            |()| {
-                #[cfg(not(feature = "async-std"))]
-                {
-                    runtime.block_on(async move {
-                        let mut handles = vec![];
-                        for _ in 0..small {
-                            let (_, handler) = Actor::spawn(None, BenchActor, ())
-                                .await
-                                .expect("Failed to create test agent");
-                            handles.push(handler);
-                        }
-                        handles
-                    })
-                }
-                #[cfg(feature = "async-std")]
-                {
-                    async_std::task::block_on(async move {
-                        let mut handles = vec![];
-                        for _ in 0..small {
-                            let (_, handler) = Actor::spawn(None, BenchActor, ())
-                                .await
-                                .expect("Failed to create test agent");
-                            handles.push(handler);
-                        }
-                        handles
-                    })
-                }
-            },
-            BatchSize::PerIteration,
-        );
-    });
-
-    let id = format!("Creation of {large} actors");
-    #[cfg(not(feature = "async-std"))]
-    let runtime = tokio::runtime::Builder::new_multi_thread().build().unwrap();
-    #[cfg(feature = "async-std")]
-    let _ = async_std::task::block_on(async {});
-    c.bench_function(&id, move |b| {
-        b.iter_batched(
-            || {},
-            |()| {
-                #[cfg(not(feature = "async-std"))]
-                {
-                    runtime.block_on(async move {
-                        let mut handles = vec![];
-                        for _ in 0..large {
-                            let (_, handler) = Actor::spawn(None, BenchActor, ())
-                                .await
-                                .expect("Failed to create test agent");
-                            handles.push(handler);
-                        }
-                        handles
-                    })
-                }
-                #[cfg(feature = "async-std")]
-                {
-                    async_std::task::block_on(async move {
-                        let mut handles = vec![];
-                        for _ in 0..large {
-                            let (_, handler) = Actor::spawn(None, BenchActor, ())
-                                .await
-                                .expect("Failed to create test agent");
-                            handles.push(handler);
-                        }
-                        handles
-                    })
-                }
-            },
-            BatchSize::PerIteration,
-        );
-    });
+    {
+        async_std::task::block_on(async {})
+    }
+    for size in [100, 10000] {
+        let id = format!("Creation of {size} actors");
+        c.bench_function(&id, |b| {
+            b.iter_batched(
+                || {},
+                |()| {
+                    #[cfg(not(feature = "async-std"))]
+                    {
+                        runtime.block_on(async move {
+                            let mut handles = vec![];
+                            for _ in 0..size {
+                                let (_, handler) = Actor::spawn(None, BenchActor, ())
+                                    .await
+                                    .expect("Failed to create test agent");
+                                handles.push(handler);
+                            }
+                            handles
+                        })
+                    }
+                    #[cfg(feature = "async-std")]
+                    {
+                        async_std::task::block_on(async move {
+                            let mut handles = vec![];
+                            for _ in 0..size {
+                                let (_, handler) = Actor::spawn(None, BenchActor, ())
+                                    .await
+                                    .expect("Failed to create test agent");
+                                handles.push(handler);
+                            }
+                            handles
+                        })
+                    }
+                },
+                BatchSize::PerIteration,
+            );
+        });
+    }
 }
 
 fn schedule_work(c: &mut Criterion) {
-    let small = 100;
-    let large = 1000;
-
-    let id = format!("Waiting on {small} actors to process first message");
     #[cfg(not(feature = "async-std"))]
     let runtime = tokio::runtime::Builder::new_multi_thread().build().unwrap();
     #[cfg(feature = "async-std")]
-    let _ = async_std::task::block_on(async {});
-    c.bench_function(&id, move |b| {
-        b.iter_batched(
-            || {
-                #[cfg(not(feature = "async-std"))]
-                {
-                    runtime.block_on(async move {
-                        let mut join_set = ractor::concurrency::JoinSet::new();
+    {
+        async_std::task::block_on(async {})
+    }
+    for size in [100, 1000] {
+        let id = format!("Waiting on {size} actors to process first message");
+        c.bench_function(&id, |b| {
+            b.iter_batched(
+                || {
+                    #[cfg(not(feature = "async-std"))]
+                    {
+                        runtime.block_on(async move {
+                            let mut join_set = ractor::concurrency::JoinSet::new();
 
-                        for _ in 0..small {
-                            let (_, handler) = Actor::spawn(None, BenchActor, ())
-                                .await
-                                .expect("Failed to create test agent");
-                            join_set.spawn(handler);
-                        }
-                        join_set
-                    })
-                }
-                #[cfg(feature = "async-std")]
-                {
-                    async_std::task::block_on(async move {
-                        let mut join_set = ractor::concurrency::JoinSet::new();
+                            for _ in 0..size {
+                                let (_, handler) = Actor::spawn(None, BenchActor, ())
+                                    .await
+                                    .expect("Failed to create test agent");
+                                join_set.spawn(handler);
+                            }
+                            join_set
+                        })
+                    }
+                    #[cfg(feature = "async-std")]
+                    {
+                        async_std::task::block_on(async move {
+                            let mut join_set = ractor::concurrency::JoinSet::new();
 
-                        for _ in 0..small {
-                            let (_, handler) = Actor::spawn(None, BenchActor, ())
-                                .await
-                                .expect("Failed to create test agent");
-                            join_set.spawn(handler);
-                        }
-                        join_set
-                    })
-                }
-            },
-            |mut handles| {
-                #[cfg(not(feature = "async-std"))]
-                {
-                    runtime.block_on(async move { while handles.join_next().await.is_some() {} })
-                }
-                #[cfg(feature = "async-std")]
-                {
-                    async_std::task::block_on(async move {
-                        while handles.join_next().await.is_some() {}
-                    })
-                }
-            },
-            BatchSize::PerIteration,
-        );
-    });
-
-    let id = format!("Waiting on {large} actors to process first message");
-    #[cfg(not(feature = "async-std"))]
-    let runtime = tokio::runtime::Builder::new_multi_thread().build().unwrap();
-    #[cfg(feature = "async-std")]
-    let _ = async_std::task::block_on(async {});
-    c.bench_function(&id, move |b| {
-        b.iter_batched(
-            || {
-                #[cfg(not(feature = "async-std"))]
-                {
-                    runtime.block_on(async move {
-                        let mut join_set = ractor::concurrency::JoinSet::new();
-                        for _ in 0..large {
-                            let (_, handler) = Actor::spawn(None, BenchActor, ())
-                                .await
-                                .expect("Failed to create test agent");
-                            join_set.spawn(handler);
-                        }
-                        join_set
-                    })
-                }
-                #[cfg(feature = "async-std")]
-                {
-                    async_std::task::block_on(async move {
-                        let mut join_set = ractor::concurrency::JoinSet::new();
-                        for _ in 0..large {
-                            let (_, handler) = Actor::spawn(None, BenchActor, ())
-                                .await
-                                .expect("Failed to create test agent");
-                            join_set.spawn(handler);
-                        }
-                        join_set
-                    })
-                }
-            },
-            |mut handles| {
-                #[cfg(not(feature = "async-std"))]
-                {
-                    runtime.block_on(async move { while handles.join_next().await.is_some() {} })
-                }
-                #[cfg(feature = "async-std")]
-                {
-                    async_std::task::block_on(async move {
-                        while handles.join_next().await.is_some() {}
-                    })
-                }
-            },
-            BatchSize::PerIteration,
-        );
-    });
+                            for _ in 0..size {
+                                let (_, handler) = Actor::spawn(None, BenchActor, ())
+                                    .await
+                                    .expect("Failed to create test agent");
+                                join_set.spawn(handler);
+                            }
+                            join_set
+                        })
+                    }
+                },
+                |mut handles| {
+                    #[cfg(not(feature = "async-std"))]
+                    {
+                        runtime
+                            .block_on(async move { while handles.join_next().await.is_some() {} })
+                    }
+                    #[cfg(feature = "async-std")]
+                    {
+                        async_std::task::block_on(async move {
+                            while handles.join_next().await.is_some() {}
+                        })
+                    }
+                },
+                BatchSize::PerIteration,
+            );
+        });
+    }
 }
 
 #[allow(clippy::async_yields_async)]
@@ -287,7 +199,9 @@ fn process_messages(c: &mut Criterion) {
     #[cfg(not(feature = "async-std"))]
     let runtime = tokio::runtime::Builder::new_multi_thread().build().unwrap();
     #[cfg(feature = "async-std")]
-    let _ = async_std::task::block_on(async {});
+    {
+        async_std::task::block_on(async {})
+    }
     c.bench_function(&id, move |b| {
         b.iter_batched(
             || {
